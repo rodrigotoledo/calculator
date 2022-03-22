97ad346d8786fe01f3139bd28899c6a111b98494
Rodrigo Toledo <rodrigotoledo@MacBook-RToledo-dev-6.local>
2022-03-22 17:40:08 -0300

    refactoring to remove instance method

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -4,34 +4,34 @@ def calculate(operation)
   operator = operation[1]
   numbers = operation.split(operator).map(&:to_i)
   operators = {
-    "+": Sum.new,
-    "-": Sub.new,
-    "*": Multi.new,
-    "/": Divi.new
+    "+": Sum,
+    "-": Sub,
+    "*": Multi,
+    "/": Divi
   }
   operators[operator.to_sym].execute(numbers)
 end
 
 class Sum
-  def execute(numbers = [])
+  def self.execute(numbers = [])
     numbers[0] + numbers[1]
   end
 end
 
 class Sub
-  def execute(numbers = [])
+  def self.execute(numbers = [])
     numbers[0] - numbers[1]
   end
 end
 
 class Multi
-  def execute(numbers = [])
+  def self.execute(numbers = [])
     numbers[0] * numbers[1]
   end
 end
 
 class Divi
-  def execute(numbers = [])
+  def self.execute(numbers = [])
     numbers[0] / numbers[1]
   end
 end
